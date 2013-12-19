4c048ada9aa4786f9c343e564ee3950cc11cc484
Ernie Hershey <ernie.hershey@10gen.com>
2013-12-19 22:11:17 +0000

    code review updates

--- buildscripts/consolidate-repos-enterprise.sh
+++ buildscripts/consolidate-repos-enterprise.sh
@@ -22,42 +22,37 @@ do
   mkdir -p "$repodir/$distro"
 done
 
-tempfile=`mktemp /tmp/consolidate-repos-enterprise.XXXXXX`
-
 mkdir -p "$repodir"
-
-find "$source_dir" -name \*.rpm -o -name \*.deb -o -name Release | grep -v "$repodir" > "$tempfile"
 
 echo "Scanning and copying package files from $source_dir"
 echo ". = skipping existing file, @ = copying file"
-while read package
+for package in $(find "$source_dir" -not \( -path "$repodir" -prune \) -and \( -name \*.rpm -o -name \*.deb -o -name Release \))
 do
-  new_package_location="$repodir`echo \"$package\" | sed 's/\/var\/www-enterprise\/[^\/]*//;'`"
-
+  new_package_location="$repodir$(echo "$package" | sed 's/\/var\/www-enterprise\/[^\/]*//;')"
   # skip if the directory structure looks weird
   #
-  if [ "`echo \"$new_package_location\" | grep /repo/`" ]
+  if echo "$new_package_location" | grep -q /repo/
   then
     continue
   fi  
 
   # skip if not enterprise package
   #
-  if [ ! "`echo \"$new_package_location\" | grep enterprise`" ]
+  if ! echo "$new_package_location" | grep -q enterprise
   then
     continue
   fi 
   # skip if it's already there 
   #
-  if [ -e "$new_package_location" -a "`basename \"$package\"`" != "Release" ]
+  if [ -e "$new_package_location" -a "$(basename "$package")" != "Release" ]
   then
       echo -n .
   else
-    mkdir -p "`dirname \"$new_package_location\"`"
+    mkdir -p "$(dirname "$new_package_location")"
     echo -n @
     cp "$package" "$new_package_location"
   fi
-done < "$tempfile"
+done
 echo
 
 # packages are in place, now create metadata
@@ -76,21 +71,19 @@ do
     gzip -9c  "$arch_dir"/Packages >  "$arch_dir"/Packages.gz
   done
 
-  for release_dir in "$debian_dir"/dists/dist
-  do
-    echo "Generating Release file under $release_dir"
-    cd $release_dir
-    tempfile=`mktemp /tmp/ReleaseXXXXXX`
-    tempfile2=`mktemp /tmp/ReleaseXXXXXX`
-    mv Release $tempfile
-    head -9 $tempfile > $tempfile2
-    apt-ftparchive release . >> $tempfile2
-    cp $tempfile2 Release
-    chmod 644 Release
-    rm Release.gpg
-    echo "Signing Release file"
-    gpg -r "$gpg_recip" --no-secmem-warning -abs --output Release.gpg  Release
-  done
+  release_dir="$debian_dir"/dists/dist
+  echo "Generating Release file under $release_dir"
+  cd $release_dir
+  tempfile=$(mktemp /tmp/ReleaseXXXXXX)
+  tempfile2=$(mktemp /tmp/ReleaseXXXXXX)
+  mv Release $tempfile
+  head -9 $tempfile > $tempfile2
+  apt-ftparchive release . >> $tempfile2
+  cp $tempfile2 Release
+  chmod 644 Release
+  rm Release.gpg
+  echo "Signing Release file"
+  gpg -r "$gpg_recip" --no-secmem-warning -abs --output Release.gpg  Release
 done
 
 for redhat_dir in $(find "$repodir"/redhat -name x86_64 -o -name i386)
@@ -99,5 +92,3 @@ do
   cd "$redhat_dir"
   createrepo .
 done
-
-
