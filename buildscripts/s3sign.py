8ad3096d9337281d3a76325fede041a92652015a
Ernie Hershey <ernie.hershey@10gen.com>
2014-02-25 23:43:54 +0000

    SERVER-5455 Works

--- buildscripts/s3sign.py
+++ buildscripts/s3sign.py
@@ -10,12 +10,14 @@
 # id = "xxxxx"
 # key = "xxxxx"
 #
-# Usage: s3sign.py [ --bucket <overridden s3 bucket> ] [ --notary-server <notary server> ] [ --key-name <key name passed to notary service> ] [ --filter <filter> ]
+# Usage: s3sign.py [ --bucket <overridden s3 bucket> ] [ --notary-url <notary url> ] [ --key-name <key name passed to notary service> ] [ --filter <filter> ]
 #
 
 
 import argparse
+import json
 import os
+import requests
 import sys
 
 sys.path.append("." )
@@ -31,13 +33,15 @@ import subprocess
 #
 parser = argparse.ArgumentParser(description='Sign MongoDB S3 Files')
 parser.add_argument('--bucket', required = False, help='Override bucket in settings.py', default = settings.bucket);
-parser.add_argument('--notary-server', required=False, help='Server for notary service', default = 'localhost');
-parser.add_argument('--key-name', required=False, help='Key parameter to notary service', default = None);
+parser.add_argument('--notary-url', required=False, help='URL base for notary service', default = 'http://localhost:5000');
+parser.add_argument('--key-name', required=False, help='Key parameter to notary service', default = 'test');
 parser.add_argument('--filter', required=False, 
                     help='Only sign files matching case-insensitive substring filter', default = None);
 args = parser.parse_args()
 
-
+notary_urlbase = args.notary_url
+notary_url = notary_urlbase + '/api/sign'
+notary_payload = { 'key': args.key_name, 'comment': 'Automatic archive signing'}
 
 # check s3 for pgp signatures
 
@@ -50,7 +54,17 @@ def check_dir( bucket , prefix ):
             pass
         elif key.endswith(".tgz" ) or key.endswith(".zip" ) or key.endswith(".tar.gz" ) or key.endswith("md5"):
             # generate signature
-            zips[key] = etag.replace( '"' , '' )
+            files = {'file': (key, bucket.get(key))}
+            r = requests.post(notary_url, files=files, data=notary_payload)
+            # get url for signature file
+            response_json = json.loads(r.text)
+            if response_json.get('permalink'):
+              signature_url = json.loads(r.text)['permalink']
+              signature = requests.get(notary_urlbase + signature_url).text
+              zips[key] = signature
+            else:
+              print('error signing %s:' % key)
+              print response_json.get('message')
         elif key.endswith(".sig" ) or key.endswith(".asc" ):
             sigs[key] = True
         elif key.endswith(".msi" ):
@@ -65,8 +79,8 @@ def check_dir( bucket , prefix ):
         if m in sigs:
             continue
 
-        print("need to do: " + x + " " + zips[x] + " to " + m )
-        # bucket.put( m , zips[x] , acl="public-read" )
+        print("need to do: " + x + " " + " to " + m )
+        bucket.put( m , zips[x] , acl="public-read" )
 
 
 def run():
