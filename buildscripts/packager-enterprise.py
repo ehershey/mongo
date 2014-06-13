cee3c94507515648d14f70d70028ca333432929f
Ernie Hershey <ernie.hershey@10gen.com>
2014-06-13 19:25:05 -0400

    more debian progress

--- buildscripts/packager-enterprise.py
+++ buildscripts/packager-enterprise.py
@@ -175,12 +175,14 @@ class Distro(object):
 
     def build_os(self):
         """Return the build os label in the binary package to download ("rhel57" and "rhel62"
-        for redhat, "ubuntu1204" for Ubuntu and Debian)"""
+        for redhat, "ubuntu1204" for Ubuntu and "debian71" for Debian)"""
 
-        if re.search("^(debian|ubuntu)", self.n):
-            return [ "ubuntu1204" ]
-        elif re.search("(redhat|fedora|centos)", self.n):
+        if re.search("(redhat|fedora|centos)", self.n):
             return [ "rhel62", "rhel57" ]
+        elif self.n == 'ubuntu':
+            return [ "ubuntu1204" ]
+        elif self.n == 'debian':
+            return [ "debian71" ]
         else:
             raise Exception("BUG: unsupported platform?")
 
